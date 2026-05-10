39a26ec4c0db8acdf6f25e2b7dd227152fde9069
luochenhao <3453851623@qq.com>
2026-05-10 19:09:04 +0800

    update.

---
+++ test/npcap/main.c
@@ -0,0 +1,18 @@
+/**
+ * @author: luochenhao
+ * @email: lch2022fox@163.com
+ * @time: Sun 10 May 2026 18:42:15 CST
+ * @brief: 利用npcap操作网卡发送和接收数据包
+*/
+
+#include <stdio.h>
+#include "sys_plat.h"
+
+int main() {
+    pcap_t* pcap = pcap_device_open();
+
+
+    printf("Hello world")
+    return 0;
+}
+
